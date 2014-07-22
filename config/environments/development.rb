7e1fbecab7717b5fa944f1032b92c090abf9360b
jnappy <jpnappy@gmail.com>
2014-07-22 19:52:03 -0400

    fixed index, new, edit, for referrals to include referrers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  #YAML.load(File.read('config/local_env.yml')).each {|k, v|  ENV[k.to_s] = v}
+  # YAML.load(File.read('config/local_env.yml')).each {|k, v|  ENV[k.to_s] = v}
   
   config.action_mailer.smtp_settings = {
     address: ENV["EMAIL_REFERRAL_SENDER_ADDRESS"],
