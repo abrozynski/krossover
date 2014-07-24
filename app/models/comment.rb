df950a4c62f6475042fb7511fced39b3a214c9f2
Gary Spagnoli <gary.spagnoli@gmail.com>
2014-07-24 15:12:00 -0400

    added kredits together- thank you rich

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
-
-  after_create :award_user
-
-  def award_user
-#  	 current_user.kredits += 5
-  end
+  
+  
+#  after_create :award_user
 
 end
