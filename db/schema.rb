c324eda397bb7f9b5c8163db597964fb255a01f1
Gary Spagnoli <gary.spagnoli@gmail.com>
2014-07-29 17:49:03 -0400

    put the js and css files back to normal

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140729191118) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: true do |t|
     t.string   "title"
     t.string   "text"
@@ -48,7 +51,7 @@ ActiveRecord::Schema.define(version: 20140729191118) do
     t.integer  "user_id"
   end
 
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
   create_table "kinds", force: true do |t|
     t.string   "name"
@@ -113,7 +116,7 @@ ActiveRecord::Schema.define(version: 20140729191118) do
     t.integer  "kredit",                 default: 0
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
