538733055491ce9dbfccb9a42158c4648dc2f1d3
jnappy <jpnappy@gmail.com>
2014-08-07 14:38:21 -0400

    added ckeditor, still need to modify so does not display html tags

--- public/assets/ckeditor/config.js
+++ public/assets/ckeditor/config.js
@@ -1,1 +1,34 @@
-CKEDITOR.editorConfig=function(e){e.filebrowserBrowseUrl="/ckeditor/attachment_files",e.filebrowserFlashBrowseUrl="/ckeditor/attachment_files",e.filebrowserFlashUploadUrl="/ckeditor/attachment_files",e.filebrowserImageBrowseLinkUrl="/ckeditor/pictures",e.filebrowserImageBrowseUrl="/ckeditor/pictures",e.filebrowserImageUploadUrl="/ckeditor/pictures",e.filebrowserUploadUrl="/ckeditor/attachment_files",e.filebrowserParams=function(){for(var e,r,t,o=document.getElementsByTagName("meta"),a=new Object,i=0;i<o.length;i++)switch(t=o[i],t.name){case"csrf-token":e=t.content;break;case"csrf-param":r=t.content;break;default:continue}return void 0!==r&&void 0!==e&&(a[r]=e),a},e.addQueryString=function(e,r){var t=[];if(!r)return e;for(var o in r)t.push(o+"="+encodeURIComponent(r[o]));return e+(-1!=e.indexOf("?")?"&":"?")+t.join("&")},CKEDITOR.on("dialogDefinition",function(r){var t,o,a=r.data.name,i=r.data.definition;CKEDITOR.tools.indexOf(["link","image","attachment","flash"],a)>-1&&(t=i.getContents("Upload")||i.getContents("upload"),o=null==t?null:t.get("upload"),o&&o.filebrowser&&void 0===o.filebrowser.params&&(o.filebrowser.params=e.filebrowserParams(),o.action=e.addQueryString(o.action,o.filebrowser.params)))})};
+
+CKEDITOR.editorConfig = function(config) {
+  config.language = 'en';
+  config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+  config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+  config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+  config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+  config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+  config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+  config.toolbar_Pure = [
+    '/', {
+      name: 'basicstyles',
+      items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
+    }, {
+      name: 'paragraph',
+      items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']
+    }, {
+      name: 'links',
+      items: ['Link', 'Unlink']
+    }, '/', {
+      name: 'styles',
+      items: ['Styles', 'Format', 'Font', 'FontSize']
+    }, {
+      name: 'colors',
+      items: ['TextColor', 'BGColor']
+    }, {
+      name: 'insert',
+      items: ['Image', 'Table', 'HorizontalRule', 'PageBreak']
+    }
+  ];
+  config.toolbar = 'Pure';
+  return true;
+};
