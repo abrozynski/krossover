4da07ef4b9c7e6a487a5714f273e8162b9d45a1a
jnappy <jpnappy@gmail.com>
2014-08-12 08:41:02 -0400

    working on api...

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,47 @@ class RegistrationsController < Devise::RegistrationsController
     #     '/users/#{current_user.slug}'
     # end
 
+  def email_checker(email)
+        YAML.load(File.read('config/environments/local_env.yml')).each {|k, v|  ENV[k.to_s] = v}
+        # include HTTParty   
+        # default_params :output => 'json'
+        # format :json
+        # base_uri 'app.close.io'
+        # basic_auth ENV["API_KEY"], ' '
+        response = HTTParty.get('https://app.close.io/api/v1/contact/', :basic_auth => {:username => ENV["API_KEY"], :password => ' '})
+        debugger
+        debugger
+        @email_database = []
+        response['data'].each do |x| 
+          x['emails'].each do |contact_info|
+              @email_database << contact_info['email']
+            end
+        end
+        debugger
+        debugger
+        unless @email_database.include? email 
+          # errors.add email, 'According to our records, you are not a Krossover customer.  Sign up at Krossover.com!'
+          return false
+        else
+            return true
+        end
+  
+  end 
+
+
+  def create
+    email = params["user"]["email"]
+    is_in_database = email_checker(email)
+    debugger
+    debugger
+    return unless is_in_database
+    super
+    # before_create :email_checker
+    
+  end
+
+# before_save :email_checker
+
 
 
   private
