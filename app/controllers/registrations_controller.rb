294bfa144f997662d63bd4d89da7c5de59f2fef8
jnappy <jpnappy@gmail.com>
2014-08-21 21:20:09 -0400

    fixed coaching bio

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -98,7 +98,7 @@ class RegistrationsController < Devise::RegistrationsController
     private
 
     def sign_up_params
-      allow = [:email, :first_name, :last_name, :school_name, :sport, :kredits, :password, :password_confirmation, :picture]
+      allow = [:email, :first_name, :last_name, :school_name, :sport, :kredits, :password, :password_confirmation, :picture, :bio]
       params.require(:user).permit(allow)
     end
 
